43ecb48c357daea875cfc3aca466b5667b965433
Greg Wilding <gregory.wilding@ucalgary.ca>
2023-02-21 15:27:26 -0700

    Checking for authorization

--- checkAuth.js
+++ checkAuth.js
@@ -3,10 +3,13 @@
 const WebSocket = require("ws"); 
 
 
-// 
+const noEEGclientId = '0ZTPAZJI7zbUWvYZusWWKYAh1mHNk95oZ0mWtGj3'; 
+const noEEGclientSecret = '2FKxZnyOI7YvrslJk1MJv6eqBj8a9nOz9HF9fqmRO8mlnmufXLFH6h905jvcSwWw1CL0CB9J15Fb1TlnkHaIt8L9qdMrQBJd0CHucNPK2iOzjvfe7eDczs2U12RqOIjR'; 
+const url = "wss://localhost:6868";
+
+// this is a messy hack to avoid root certificate
 process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = 0;
 
-const url = "wss://localhost:6868";
 
 let socket = new WebSocket(url);
 
@@ -19,7 +22,14 @@ function sendMessage(id, method, params) {
 socket.on('open', async () => {
 
    console.log("sending request");
+
    sendMessage(1, "getUserLogin", null);
+
+   let clientInfo = {
+      "clientId": noEEGclientId,
+      "clientSecret": noEEGclientSecret
+   };
+   sendMessage(2, "requestAccess", clientInfo);
 
 });
 
